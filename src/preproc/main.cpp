f81c09a565fd2f1e468b8469fabf274ac3304169
Sung-Soo Son <sungsu7437@naver.com>
2016-07-05 15:06:03 -0700

    need more improve in file read section

--- src/preproc/main.cpp
+++ src/preproc/main.cpp
@@ -3,18 +3,26 @@
 void delFiles();
 
 int main(int argc, char *argv[]) {
-	int size = 100000;						//test size for each partitions
+	int size = 5000000;						//test size for each partitions
+	clock_t begin, end;
 
 	delFiles();
-	Preproc pre;
-	pre.makeVIT("test.txt", size);		//need to fix input is test.txt file
-	pre.makePart("test.txt");			//need to fix
-	pre.partSort();
+	Preproc pre("bigtest.txt");
+
+	begin = clock();
+	pre.makeVIT("bigtest.txt", size);		//need to fix input file
+	end = clock();
+	std::cout << "makeVIT time : " << ((end - begin) / CLOCKS_PER_SEC) << std::endl;
+
+	begin = clock();
+	pre.makePart();
+	end = clock();
+	std::cout << "makePart time : " << ((end - begin) / CLOCKS_PER_SEC) << std::endl;
 	return 0;
 }
 
 void delFiles() {//delete test files
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < 200; i++) {
 		std::string str = "part" + std::to_string(i);		//need to fix file names
 		if (std::ifstream(str))
 			std::remove(str.c_str());
