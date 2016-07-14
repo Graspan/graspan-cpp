1ddbe9ac6b3e79027d8466b3653b5d3f02c34ba5
Sem <rmsdlf159@naver.com>
2016-07-15 05:35:56 +0900

    add nextPartition & adding partition

--- src/DDM/DDM.cpp
+++ src/DDM/DDM.cpp
@@ -20,33 +20,47 @@ DDM::DDM(string dirName){
 		free(file);
 	}
 	free(filelist);
+	originNumPartition = numPartition;
 
 	//make partitionRate
 	partitionRate.assign(numPartition,vector<double>(numPartition,0));
 	edgeCnt.assign(numPartition,vector<double>(numPartition,0));
 	partitionSize.assign(numPartition,0);
 	
-	std::ifstream fin;
-	char str[512];
-	char *p;
-	for(int i=0;i<numPartition;i++){
-		fin.open(dirName + partitionName.at(i));
-
-		while(!fin.getline(str,sizeof(str)).eof()){
-			vector<int> val;
-			p = strtok(str," ");
-			while(p != NULL){
-				val.push_back(atoi(p));
-				p = strtok(NULL," ");
-			}
-			prepare(val);
+	for(int i=0;i<numPartition;i++){	
+		if(f_read(dirName + partitionName.at(i))==-1){
+			cout<< "can't open" <<endl;
+			return;
 		}
-		fin.close();
 	}
+
 	set();
 }
 
 DDM::~DDM(){}
+
+int DDM::f_read(string name){
+	std::ifstream fin;
+	char str[512];
+	char *p;
+	int src;	
+
+	fin.open(name);
+	if(!fin) return -1;
+
+	while(!fin.getline(str,sizeof(str)).eof()){
+		vector<int> val;
+		p = strtok(str," ");
+		while(p != NULL){
+			val.push_back(atoi(p));
+			p = strtok(NULL," ");
+		}
+		prepare(val);
+		src=val[0];
+	}
+	fin.close();
+	return findPart(src);
+}
 
 void DDM::prepare(vector<int> val){
 	int dst;
@@ -61,29 +75,64 @@ void DDM::prepare(vector<int> val){
 }
 
 int DDM::findPart(int num){
-	int VIT[]={3,5}; //just test It will be fixed that real VIT
+	std::vector<int> VIT; //just test It will be fixed that real VIT
+	VIT.push_back(3);
+	VIT.push_back(5);
+	VIT.push_back(7);
 
-	for(int i=0;i<2;i++)
+	for(int i=0;i<VIT.size();i++)
 		if(num <= VIT[i])	return i;
 	return -1;
 }
 
 void DDM::set(){
-	for(int i=0;i<numPartition;i++){
-		for(int j=0;j<numPartition;j++){
+	for(int i=0;i<partitionRate.size();i++){
+		for(int j=0;j<partitionRate[i].size();j++){
 			partitionRate[i][j] = edgeCnt[i][j]/partitionSize[i];
 		}
 	}
 }
 
-long DDM::nextPartitionPart(){return 1;}
+long DDM::nextPartitionPart(){
+	double max=0;
+	long store_part;	//store partition
+
+	for(int i=0;i<partitionRate.size();i++){
+		for(int j=0;j<partitionRate[i].size();j++){
+			if(max<partitionRate[i][j]){
+				max=partitionRate[i][j];
+				store_part = i << 32 | j;
+			}
+		}
+	}
+	return store_part;
+}
+
+void DDM::addPartition(string fileName){
+	numPartition++;
+	partitionRate.resize(numPartition,vector<double>(originNumPartition,0));
+	edgeCnt.resize(numPartition,vector<double>(originNumPartition,0));
+	partitionSize.resize(numPartition,0);
+
+	int src;	
+	if((src=f_read(fileName))==-1){
+		cout<< "can't open" <<endl;
+		return;
+	}
+		
+	//set
+	for(int i=0;i<originNumPartition;i++)
+		partitionRate[src][i] = edgeCnt[src][i] / partitionSize[src];
+}
 
 void DDM::testPrint(){
-	for(int i=0;i<numPartition;i++){
-		for(int j=0; j<numPartition; j++){
+	for(int i=0;i<partitionRate.size();i++){
+		for(int j=0; j<partitionRate[i].size(); j++){
 			cout.precision(2);
 			cout << partitionRate[i][j] << "% ";
 		}
 		cout << endl;
 	}
+	long next = nextPartitionPart();
+	cout << "next partition are \'" << (int)(next >> 32) << "\' and \'" << (int)next << "\'." << endl;
 }
