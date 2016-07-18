19c29085e6ebb244182e114d073b55f013ef7ebc
khanhtn1 <ktruong.nguyen@gmail.com>
2016-07-18 00:39:57 -0700

    DDM quick review

--- src/DDM/DDM.cpp
+++ src/DDM/DDM.cpp
@@ -1,5 +1,11 @@
 #include "DDM.h"
 
+DDM::DDM() {
+  //to be implemented
+  // the intial vector should be reserved quite big to accomondate for repartitioning
+
+}
+
 DDM::DDM(int numPartition){
 	this->numPartition = numPartition;
 	originNumPartition = numPartition;
@@ -9,32 +15,59 @@ DDM::DDM(int numPartition){
 
 DDM::~DDM(){}
 
-void DDM::set(int p,int q,double rate){
-	partitionRate[p][q] = rate;
-}
+//void DDM::set(int p, int q, double rate){
+//	partitionRate[p][q] = rate;
+//}
 
-//if using this func, you should divide 2 int value
+//if using this func, you should divide 2 int value -- bad way to do this
 long DDM::nextPartitionPart(){
 	double max=0;
-	long store_part;	//store partition
+	long store_part=0;	//store partition
 
 	for(int i=0;i<partitionRate.size();i++){
 		for(int j=0;j<partitionRate[i].size();j++){
 			if(max<partitionRate[i][j]){
 				max=partitionRate[i][j];
-				store_part = i << 32 | j;
+				store_part = ((long)i << 32) | j; // cast to long first otherwise you will delete value of i
 			}
 		}
 	}
 	return store_part;
 }
 
-void DDM::adjust(int p){
-	numPartition++;
-	partitionRate.resize(numPartition,vector<double>(originNumPartition,0));
-	terminate_map.resize(numPartition,vector<int>(originNumPartition,UNMARK));
+void DDM::adjust(partitionid_t p){
+	//numPartition++;
+	//partitionRate.resize(numPartition,vector<double>(originNumPartition,0));
+	//terminate_map.resize(numPartition,vector<int>(originNumPartition,UNMARK));
+
+  //this is not correct - the meaning of adjust p is to set teminate_map[p][] and teminate_map[][p] into UNMARK
 }
 
-void DDM::markTerminate(int p,int q){
-	terminate_map[p][q] = MARK;
+//return false iff the whole system should terminate
+bool DDM::nextPartitionPair(partitionid_t &p, partitionid_t &q) {
+
+  //TODO: we want to favor the current loaded partition in memory: how to express that? (i.e., add bonus to the score)
+  double max = 0.0;
+
+  for (int i = 0; i < numPartition; ++i) {
+    for (int j = 0; j < numPartition; ++j ) {
+      if (max < partitionRate[i][j]){
+        max=partitionRate[i][j];
+        p = i;
+        q = j;
+      }
+    }
+
+  }
+
+  if (max == 0.0) { // should terminate since every cell is either 0 or negative -- we assume a pair is done when its partitionRate is negative
+    return false;
+  }
+  
+  return true;
 }
+
+void DDM::enlarge() {
+  //to be implemented
+}
+
--- src/DDM/DDM.h
+++ src/DDM/DDM.h
@@ -1,20 +1,48 @@
+//where is my #ifndef and #def?
+
+#include "../utilities/globalDefinitions.hpp"
 #include <iostream>
-#include <vector>
+
 
 #define MARK 1
 #define UNMARK 0
 
+// There should be only one DDM object in the whole execution
+// ==> implement Singleton pattern
+
 class DDM{
-private:
+ private:
+  //TODO: merge these two vectors into 1
+  //TODO: implement scheduling policy described in the paper
 	vector<vector<double> > partitionRate; //for store percentage
-	vector<vector<int> > terminate_map;
+	vector<vector<int> > terminate_map; //why int instead of bool??
 	int numPartition;
-	int originNumPartition;
-public:
-	DDM(int numPartition);
+	int originNumPartition; //what's this for?
+
+ public:
+  
+  //default constructor, the numPartition should be obtained thru the Context object
+  DDM(); 
+  DDM(int numPartition); 
 	~DDM();
-	void set(int p,int q,double rate);
-	long nextPartitionPart();
+
+  //small methods should be inlined for efficiency
+	inline void set(partitionid_t p, partitionid_t q, double rate) {
+    partitionRate[p][q] = rate;
+  }
+	long nextPartitionPart(); // this is a bad way to return 2 values since you spent time encoding and decoding
 	void adjust(int p);
-	void markTerminate(int p,int q);
+
+	inline void markTerminate(partitionid_t p, partitionid_t q) {
+    terminate_map[p][q] = MARK;
+  }
+
+  // the indices p and q tell which partitionRate[p][q] is max: i.e., this is our scheduler
+  // the bool return value indicate whether or not we should terminate the whole system
+  bool nextPartitionPair(int& p, int &q);
+
+  // if repartitioning happens, 1 or 2 new partition can happen, that means DDM needs to be enlarged
+  // you should figure out how to implement this resizing logic efficiently
+  void enlarge();
+  
 };
