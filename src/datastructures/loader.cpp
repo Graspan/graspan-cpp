5c652e4e10da87811d23cf1f7b2cbc35cd173a11
sungsoos <sungsoos@buckeye.ics.uci.edu>
2016-07-26 13:33:58 -0700

    loader fixed

--- src/datastructures/loader.cpp
+++ src/datastructures/loader.cpp
@@ -17,77 +17,122 @@ bool Loader::loadPartition(int id, Partition &p, bool readable)
 	int i = 0;
 	string str;
 	char *ctemp[2];
-	char buf[512];
+	char buf[4096];
 	char *p_token = NULL;
 	char *context = NULL;
 	if (readable) {
 		str = GRAP + "." + PART + "." + HUMA + "." + std::to_string((long long)id);
 		fp = fopen(str.c_str(), "r");
+		int linecounts = 0;
+		if (fp!= NULL) {
+			while (NULL != fgets(buf, sizeof(buf), fp)) {
+				vector<vertexid_t> outEdges;
+				vector<label_t> outEdgeValues;
 
-		while (NULL != fgets(buf, sizeof(buf), fp)) {
-			vector<vertexid_t> outEdges;
-			vector<label_t> outEdgeValues;
+				p_token = strtok_r(buf, "\n", &context);
+				if (p_token == NULL) {
+					cout << "NULL ptoken linecount value = " << linecounts << " : " << str << endl;
+					break;
+				}
+				if (!strcmp(p_token, "") || !strcmp(p_token, " ")) {//if (p_token == "" || p_token == " ")
+					cout << "linecount value = " << linecounts << " : " << str <<  endl;
+					break;
+				}
+				
+				linecounts++;
+				
+				p_token = strtok_r(buf, "\t", &context);
+				if (p_token == NULL) {
+					cout << "2nd check NULL ptoken linecount value = " << linecounts << " : " << str << endl;
+					break;
+				}
+				if (!strcmp(p_token, "") || !strcmp(p_token, " ")) {//if (p_token == "" || p_token == " ")
+					cout << "2nd check linecount value = " << linecounts << " : " << str << endl;
+					break;
+				}
 
-			p_token = strtok_r(buf, "\n", &context);
-			p_token = strtok_r(buf, "\t", &context);
-			src = atoi(p_token);
+				src = atoi(p_token);
 
-			p_token = strtok_r(NULL, "\t", &context);
-			degree = atoi(p_token);
-			size += degree;
-			numVertices++;
-
-			p_token = strtok_r(NULL, "\t", &context);
-			while (p_token != NULL) {
-				ctemp[i++] = p_token;
 				p_token = strtok_r(NULL, "\t", &context);
+				if (p_token == NULL) {
+					cout << "3rd check NULL ptoken linecount value = " << linecounts << " : " << str << endl;
+					break;
+				}
+				if (!strcmp(p_token, "") || !strcmp(p_token, " ")) {//if (p_token == "" || p_token == " ")
+					cout << "3rd check linecount value = " << linecounts << " : " << str << endl;
+					break;
+				}
+				degree = atoi(p_token);
+				size += degree;
+				numVertices++;
 
-        // you don't have to do this right? you know which vector to push 
-        // one vector has only elements whose index is odd, the other, even
-        // that eliminate the char[2]
-				if (i == 2) {
-					outEdges.push_back(atoi(ctemp[0]));
-					outEdgeValues.push_back(atoi(ctemp[1]));
-					label = atoi(ctemp[1]);
-					i = 0;
-				} 
+				p_token = strtok_r(NULL, "\t", &context);
+				int i = 0;
+				while (p_token != NULL ) {
+					ctemp[i++] = p_token;
+					assert(p_token != "", "blank token" );
+					assert(p_token != " ", "space token" );
+					//assert(p_token != "\s", "anyspace token" );
+					// you don't have to do this right? you know which vector to push 
+					// one vector has only elements whose index is odd, the other, even
+					/*if (i % 2 ==  1) {
+						outEdgeValues.push_back(atoi(p_token));
+					}
+					else {
+						outEdges.push_back(atoi(p_token));
+					}*/
+					p_token = strtok_r(NULL, "\t", &context);
+					//i++;
+					if						(i == 2) {
+						outEdges.push_back(atoi(ctemp[0]));
+						outEdgeValues.push_back(atoi(ctemp[1]));
+						//label = atoi(ctemp[1]);
+						i = 0;
+					}
+				}
+				Vertex v(0, src, outEdges, outEdgeValues);
+				data.push_back(v);
 			}
-			Vertex v(0, src, outEdges, outEdgeValues);
-			data.push_back(v);
+			p.setID(id);
+			p.setNumEdges(size);
+			p.setNumVertices(numVertices);
+			fclose(fp);
+			return 1;
+		} else {
+			assert(false, "Cannot open human file " + id);
 		}
-		p.setID(id);
-		p.setNumEdges(size);
-		p.setNumVertices(numVertices);
-		fclose(fp);
-		return 1;
 	}
 	else {
 		str = GRAP + "." + PART + "." + BINA + "." + std::to_string((long long)id);
 		fp = fopen(str.c_str(), "rb");
+		if (fp != NULL) {
+			// wouldnt this cause problem with hard-coded value 4 and 1???
+			while (0 != fread(&src, 4, 1, fp)) {
+				vector<vertexid_t> outEdges;
+				vector<label_t> outEdgeValues;
+				fread(&degree, 4, 1, fp);
+				size += degree;
+				numVertices++;
+				for (int i = 0; i < degree; i++) {
+					fread(&dst, 4, 1, fp);
 
-    // wouldnt this cause problem with hard-coded value 4 and 1???
-		while (0 != fread(&src, 4, 1, fp)) {
-			vector<vertexid_t> outEdges;
-			vector<label_t> outEdgeValues;
-			fread(&degree, 4, 1, fp);
-			size += degree;
-			numVertices++;
-			for (int i = 0; i < degree; i++) {
-				fread(&dst, 4, 1, fp);
+					fread(&label, 1, 1, fp);
 
-				fread(&label, 1, 1, fp);
-
-				outEdges.push_back(dst);
-				outEdgeValues.push_back(label);
+					outEdges.push_back(dst);
+					outEdgeValues.push_back(label);
+				}
+				Vertex v(0, src, outEdges, outEdgeValues);
+				data.push_back(v);
 			}
-			Vertex v(0, src, outEdges, outEdgeValues);
-			data.push_back(v);
+			p.setID(id);
+			p.setNumEdges(size);
+			p.setNumVertices(numVertices);
+			fclose(fp);
+			return 1;
 		}
-		p.setID(id);
-		p.setNumEdges(size);
-		p.setNumVertices(numVertices);
-		fclose(fp);
-		return 1;
+		else {
+			assert(false, "cant open bin file " + id);
+		}
 	}
 	return 0; // this is dead code!!!
 }
